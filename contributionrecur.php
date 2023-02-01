afafb51058a1e0a06f915d449eace74360548126
Alan Dixon <alan.g.dixon@gmail.com>
2023-02-01 18:23:34 +0000

    civix-generated upgrade

--- contributionrecur.php
+++ contributionrecur.php
@@ -12,17 +12,6 @@ function contributionrecur_civicrm_config(&$config) {
 }
 
 /**
- * Implementation of hook_civicrm_xmlMenu
- *
- * @param $files array(string)
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_xmlMenu
- */
-function contributionrecur_civicrm_xmlMenu(&$files) {
-  _contributionrecur_civix_civicrm_xmlMenu($files);
-}
-
-/**
  * Implementation of hook_civicrm_install
  *
  * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_install
@@ -32,45 +21,12 @@ function contributionrecur_civicrm_install() {
 }
 
 /**
- * Implementation of hook_civicrm_uninstall
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_uninstall
- */
-function contributionrecur_civicrm_uninstall() {
-  _contributionrecur_civix_civicrm_uninstall();
-}
-
-/**
  * Implementation of hook_civicrm_enable
  *
  * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_enable
  */
 function contributionrecur_civicrm_enable() {
   _contributionrecur_civix_civicrm_enable();
-}
-
-/**
- * Implementation of hook_civicrm_disable
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_disable
- */
-function contributionrecur_civicrm_disable() {
-  _contributionrecur_civix_civicrm_disable();
-}
-
-/**
- * Implementation of hook_civicrm_upgrade
- *
- * @param $op string, the type of operation being performed; 'check' or 'enqueue'
- * @param $queue CRM_Queue_Queue, (for 'enqueue') the modifiable list of pending up upgrade tasks
- *
- * @return mixed  based on op. for 'check', returns array(boolean) (TRUE if upgrades are pending)
- *                for 'enqueue', returns void
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_upgrade
- */
-function contributionrecur_civicrm_upgrade($op, CRM_Queue_Queue $queue = NULL) {
-  return _contributionrecur_civix_civicrm_upgrade($op, $queue);
 }
 
 /**
@@ -120,29 +76,6 @@ function contributionrecur_civicrm_managed(&$entities) {
       'payment_type' => 2,
     ),
   );
-  _contributionrecur_civix_civicrm_managed($entities);
-}
-
-/**
- * Implementation of hook_civicrm_caseTypes
- *
- * Generate a list of case-types
- *
- * Note: This hook only runs in CiviCRM 4.4+.
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_caseTypes
- */
-function contributionrecur_civicrm_caseTypes(&$caseTypes) {
-  _contributionrecur_civix_civicrm_caseTypes($caseTypes);
-}
-
-/**
- * Implementation of hook_civicrm_alterSettingsFolders
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_alterSettingsFolders
- */
-function contributionrecur_civicrm_alterSettingsFolders(&$metaDataFolders = NULL) {
-  _contributionrecur_civix_civicrm_alterSettingsFolders($metaDataFolders);
 }
 
 /*
@@ -719,3 +652,21 @@ function contributionrecur_civicrm_tabset($tabsetName, &$tabs, $context) {
     );
   }
 }
+
+// /**
+//  * Implements hook_civicrm_postInstall().
+//  *
+//  * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_postInstall
+//  */
+// function contributionrecur_civicrm_postInstall() {
+//   _contributionrecur_civix_civicrm_postInstall();
+// }
+
+// /**
+//  * Implements hook_civicrm_entityTypes().
+//  *
+//  * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_entityTypes
+//  */
+// function contributionrecur_civicrm_entityTypes(&$entityTypes) {
+//   _contributionrecur_civix_civicrm_entityTypes($entityTypes);
+// }
